22ae1d35cbe8d430fb57aeebb64d5488571d5306
makise-homura <akemi_homura@kurisa.ch>
2021-06-09 22:07:08 +0300

    Added BCD image decomposition

--- kexec-lintel.c
+++ kexec-lintel.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -50,21 +51,79 @@ void free_lintel(void)
     free(lintel.image);
 }
 
-void load_lintel(const char *fname)
+void read_lintel(FILE *f, size_t realsize)
 {
-    FILE *f = fopen(fname,"r"); size_t realsize;
-    if (f == NULL) cancel(1, "Can't open %s: %s\n", fname, strerror(errno));
-    if (fseek(f, 0, SEEK_END) != 0) { fclose(f); cancel(2, "Can't seek lintel file: %s\n", strerror(errno)); }
-    if ((realsize = ftell(f)) == -1) { fclose(f); cancel(3, "Can't get file position of lintel file: %s\n", strerror(errno)); }
-    rewind(f);
     lintel.image_size = realsize + alignment; lintel.image_size -= lintel.image_size % alignment;
-
     if (posix_memalign(&lintel.image, alignment, lintel.image_size)) { fclose(f); cancel(4, "Can't allocate %ld bytes for lintel file\n", lintel.image_size); }
     atexit(free_lintel);
     if (fread(lintel.image, realsize, 1, f) != 1) { fclose(f); cancel(5, "Can't read %ld bytes for lintel file: %s\n", realsize, strerror(errno)); }
-    fclose(f);
+    printf("Loaded lintel: %ld bytes at address %p (%ld bytes aligned at 0x%x), ioctl struct at %p\n", realsize, lintel.image, lintel.image_size, alignment, &lintel);
+}
 
-    printf("Loaded lintel file: %s, %ld bytes at address %p (%ld bytes aligned at 0x%x), ioctl struct at %p\n", fname, realsize, lintel.image, lintel.image_size, alignment, &lintel);
+int bcd_check_files(FILE *f)
+{
+    if (fseek(f, 512, SEEK_SET) != 0) { fclose(f); cancel(14, "Can't seek to possible header of file: %s\n", strerror(errno)); }
+    struct __attribute__((packed))
+    {
+        uint64_t signature;
+        uint32_t files_num;
+        uint64_t free_lba;
+    } header;
+    if (fread(&header, sizeof(header), 1, f) != 1) { fclose(f); cancel(10, "Can't read header of lintel file: %s\n", strerror(errno)); }
+    if (header.signature == 0x012345678ABCDEF0ull) return header.files_num;
+    return -1;
+}
+
+void load_bcd_lintel(FILE *f, int files)
+{
+    printf ("File is BCD container (%d files).\n", files);
+
+    for (int i = 0; i < files; ++i)
+    {
+
+        struct __attribute__((packed)) xrt_BcdFile_t
+        {
+            uint64_t lba;
+            uint64_t size;
+            uint64_t init_size;
+            uint32_t tag;
+            uint32_t checksum;
+        } file;
+        if (fread(&file, sizeof(file), 1, f) != 1) { fclose(f); cancel(11, "Can't read file %d header of BCD file: %s\n", strerror(errno)); }
+        printf("BCD file %d: /%d, offset %d blocks, size %d blocks, init_size %d blocks, checksum 0x%08x\n", i, file.tag, file.lba, file.size, file.init_size, file.checksum);
+
+        if (file.tag == 0) /* 0 is the tag of Lintel binary */
+        {
+            if (file.size > file.init_size) { fclose(f); cancel(13, "Can't read lintel file from BCD file: file is uninitialized\n"); }
+            if (fseek(f, 512 * file.lba, SEEK_SET) != 0) { fclose(f); cancel(14, "Can't seek to start of lintel binary in BCD file: %s\n", strerror(errno)); }
+            read_lintel(f, 512 * file.size);
+            return;
+        }
+    }
+    fclose(f); cancel(12, "Can't find lintel file in BCD file.\n");
+}
+
+void load_raw_lintel(FILE *f)
+{
+    size_t realsize;
+    printf ("File seems to be raw lintel image.\n");
+    if (fseek(f, 0, SEEK_END) != 0) { fclose(f); cancel(2, "Can't seek lintel file: %s\n", strerror(errno)); }
+    if ((realsize = ftell(f)) == -1) { fclose(f); cancel(3, "Can't get file position of lintel file: %s\n", strerror(errno)); }
+    rewind(f);
+    read_lintel(f, realsize);
+}
+
+void load_lintel(const char *fname)
+{
+    FILE *f = fopen(fname,"r");
+    if (f == NULL) cancel(1, "Can't open %s: %s\n", fname, strerror(errno));
+    printf("Loading lintel from %s:\n", fname);
+
+    int files = bcd_check_files(f);
+    if (files == -1) load_raw_lintel(f);
+    else load_bcd_lintel(f, files);
+
+    fclose(f);
 }
 
 int check_syslog(const char *marker)
